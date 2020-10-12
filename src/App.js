a97c92ba2f7c94d1f0272f5388d4d3704687e00d
Forrest Tanaka <forrest@forrest-tanaka.com>
2020-10-11 22:49:26 -0700

    Rewrite as checkbox input
    Remove TriState version because of its difficulty of use.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import * as Switch from './switch';
+import BooleanSwitch from './switch';
 import './App.scss';
-import './switch.scss';
 
 function App() {
     const [twoSwitchState, setTwoSwitchState] = React.useState(false);
-    const [triSwitchState, setTriSwitchState] = React.useState(1);
     const handleTriggerClick = () => {
         setTwoSwitchState(!twoSwitchState);
     };
 
-    const handleTriTriggerClick = (newSwitchState) => {
-        setTriSwitchState(newSwitchState);
-    }
-
     return (
         <div className="App">
             <div className="switch-demo-1">
-                <Switch.TwoState state={twoSwitchState} title="Perturbed" triggerHandler={handleTriggerClick} />
-                <Switch.TriState state={triSwitchState} title="Perturbed" name="perturbed" triggerHandler={handleTriTriggerClick} />
+                <BooleanSwitch state={twoSwitchState} title="Plugged" triggerHandler={handleTriggerClick} />
             </div>
         </div>
     );
--- src/switch.js
+++ src/switch.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './switch.scss';
 
 
 const DEFAULT_SWITCH_HEIGHT = 22;
@@ -10,7 +11,18 @@ const DEFAULT_SWITCH_HEIGHT = 22;
  */
 const DEFAULT_TWOSTATE_WIDTH = DEFAULT_SWITCH_HEIGHT * 1.6;
 const DEFAULT_TWOSTATE_HEIGHT = DEFAULT_SWITCH_HEIGHT;
-export const TwoState = ({ state, title, triggerHandler, options }) => {
+export const BooleanSwitch = ({ state, title, triggerHandler, options }) => {
+    // True if checkbox input has focus.
+    const [focused, setFocused] = React.useState(false);
+
+    const handleFocus = () => {
+        setFocused(true);
+    };
+
+    const handleBlur = () => {
+        setFocused(false);
+    };
+
     // Calculate the inline styles for the switch and trigger.
     const width = options.width || DEFAULT_TWOSTATE_WIDTH;
     const height = options.height || DEFAULT_TWOSTATE_HEIGHT;
@@ -20,27 +32,27 @@ export const TwoState = ({ state, title, triggerHandler, options }) => {
         height,
         borderRadius: height / 2,
         backgroundColor: state ? '#65afeb' : '#e9e9eb',
-        display: options.inline ? 'inline-block' : 'block',
     };
-    const switchCss = `switch${options.switchCss ? ` ${options.switchCss}` : ''}`;
-    const triggerStyles = {
+    const actuatorStyles = {
         width: triggerSize,
         height: triggerSize,
         borderRadius: (height / 2) - 2,
         top: 2,
         left: state ? (width - height) + 2 : 2,
     };
-    const triggerCss = `switch-trigger${options.triggerCss ? ` ${options.triggerCss}` : ''}`;
 
     return (
-        <button className={switchCss} style={switchStyles} aria-pressed={state} onClick={triggerHandler}>
-            <div className={triggerCss} style={triggerStyles} />
-            <div className="sr-only">{title}</div>
-        </button>
+        <label className={`boolean-switch${focused ? ' boolean-switch--focused' : ''}`}>
+            <div className="boolean-switch__title">{title}</div>
+            <div style={switchStyles} className="boolean-switch__frame">
+                <div style={actuatorStyles} className="boolean-switch__actuator" />
+            </div>
+            <input type="checkbox" checked={state} onChange={triggerHandler} onFocus={handleFocus} onBlur={handleBlur} />
+        </label>
     );
 };
 
-TwoState.propTypes = {
+BooleanSwitch.propTypes = {
     /** Current state to display in the switch */
     state: PropTypes.bool.isRequired,
     /** Title of the button for screen readers */
@@ -53,8 +65,6 @@ TwoState.propTypes = {
         width: PropTypes.number,
         /** Height of switch in pixels; 22px by default */
         height: PropTypes.number,
-        /** True to use inline-block style */
-        inline: PropTypes.bool,
         /** Extra CSS classes for switch */
         switchCss: PropTypes.string,
         /** Extra CSS classes for switch trigger */
@@ -62,121 +72,8 @@ TwoState.propTypes = {
     }),
 };
 
-TwoState.defaultProps = {
+BooleanSwitch.defaultProps = {
     options: {},
 };
 
-
-/**
- * Renders a three-state boolean switch (on, off, neither). This keeps no internal state -- the
- * state of the switch is kept by the parent component which passes it down.
- *
- * The radio-button input labels exist to receive clicks and keyboard events, but they’re
- * transparent. We use another div on top to handle the trigger animation though it receives no
- * click events -- it's there for the animation only. It has to be positioned absolutely for the
- * animation, but the radio-button input labels use flexbox to position themselves because
- * absolutely positioned labels can't receive clicks.
- *
- * You're supposed to put radio buttons in a <fieldset> but Chrome has a bug in that flexbox
- * doesn't work inside a <fieldset> element.
- */
-const DEFAULT_TRISTATE_WIDTH = DEFAULT_SWITCH_HEIGHT * 2.3;
-const DEFAULT_TRISTATE_HEIGHT = DEFAULT_SWITCH_HEIGHT;
-export const TriState = ({ state, title, name, triggerHandler, options }) => {
-    const stateNum = +state;
-    const width = options.width || DEFAULT_TRISTATE_WIDTH;
-    const height = options.height || DEFAULT_TRISTATE_HEIGHT;
-    const triggerSize = height - 8;
-    const triggerMidSize = triggerSize / 2;
-
-    // Determine the color of the switch background and switch trigger position based on the state.
-    let switchColor;
-    let switchTriggerPosition;
-    if (stateNum < 0) {
-        switchColor = '#e9e9eb';
-        switchTriggerPosition = 2;
-    } else if (stateNum > 0) {
-        switchColor = '#65afeb';
-        switchTriggerPosition = (width - triggerSize) - 6;
-    } else {
-        switchColor = '#ffd561';
-        switchTriggerPosition = (width / 2) - (triggerMidSize / 2) - 2;
-    }
-
-    // Generate the dynamic CSS styles for the switch.
-    const switchStyles = {
-        width,
-        height,
-        borderRadius: height / 2,
-        backgroundColor: switchColor,
-        display: options.inline ? 'inline-flex' : 'flex',
-    };
-    const switchCss = `tri-switch${options.switchCss ? ` ${options.switchCss}` : ''}`;
-
-    // Generate the dynamic CSS styles for the clickable labels.
-    const triggerLabelStyles = {
-        height: triggerSize,
-    };
-    const triggerLabelStylesMinus = Object.assign({}, triggerLabelStyles, { width: triggerSize, flexBasis: triggerSize, marginRight: 0, borderRadius: height / 2 });
-    const triggerLabelStylesZero = Object.assign({}, triggerLabelStyles, { width: triggerMidSize, flexBasis: triggerMidSize, marginRight: 0, marginLeft: 0, borderRadius: 2 });
-    const triggerLabelStylesPlus = Object.assign({}, triggerLabelStyles, { width: triggerSize, flexBasis: triggerSize, marginLeft: 0, borderRadius: height / 2 });
-
-    // Generate the dynamic CSS styles for the animated trigger.
-    const triggerCss = `switch-trigger${options.triggerCss ? ` ${options.triggerCss}` : ''}`;
-    const triggerStyles = {
-        left: switchTriggerPosition,
-        width: stateNum === 0 ? triggerMidSize : triggerSize,
-        height: triggerSize,
-        borderRadius: state === 0 ? 2 : (height / 2) - 2,
-    }
-
-    const inputStylesMinus = { top: (height / 2) - 8, left: 4 };
-    const inputStylesZero = { top: (height / 2) - 8, left: (width / 2) - (triggerMidSize / 2) - 2 };
-    const inputStylesPlus = { top: (height / 2) - 8, left: (width - triggerSize) - 2 };
-
-    // Send the clicked radio-button value to the parent.
-    const triggerHandlerShim = (e) => {
-        triggerHandler(e.target.value);
-    };
-
-    return (
-        <div className={switchCss} style={switchStyles}>
-            <legend className="sr-only">{title}</legend>
-            <input type="radio" name={name} id={`${name}-off`} value={-1} style={inputStylesMinus} defaultChecked={state === -1} onChange={triggerHandlerShim} />
-            <label htmlFor={`${name}-off`} style={triggerLabelStylesMinus}><div className="sr-only">off</div></label>
-            <input type="radio" name={name} id={`${name}-none`} value={0} style={inputStylesZero} defaultChecked={state === 0} onChange={triggerHandlerShim} />
-            <label htmlFor={`${name}-none`} style={triggerLabelStylesZero}><div className="sr-only">none</div></label>
-            <input type="radio" name={name} id={`${name}-on`} value={1} style={inputStylesPlus} defaultChecked={state === 1} onChange={triggerHandlerShim} />
-            <label htmlFor={`${name}-on`} style={triggerLabelStylesPlus}><div className="sr-only">on</div></label>
-            <div className={triggerCss} style={triggerStyles} />
-        </div>
-    );
-};
-
-TriState.propTypes = {
-    /** Current state to display in the switch; >0=on, 0=neither, <0=off */
-    state: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-    /** Title of the button for screen readers */
-    title: PropTypes.string.isRequired,
-    /** Name for the switch; unique on the page like id */
-    name: PropTypes.string.isRequired,
-    /** Called when the user clicks anywhere in the switch */
-    triggerHandler: PropTypes.func.isRequired,
-    /** Other styling options */
-    options: PropTypes.exact({
-        /** Width of switch in pixels; 36px by default */
-        width: PropTypes.number,
-        /** Height of switch in pixels; 22px by default */
-        height: PropTypes.number,
-        /** True to use inline-block style */
-        inline: PropTypes.bool,
-        /** Extra CSS classes for switch */
-        switchCss: PropTypes.string,
-        /** Extra CSS classes for switch trigger */
-        triggerCss: PropTypes.string,
-    }),
-};
-
-TriState.defaultProps = {
-    options: {},
-};
+export default BooleanSwitch;
